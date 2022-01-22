6d65f4c61ceba8fdf92ea6aa67f5693ab301affb
Lokie-codes <lokeshs.19ec@saividya.ac.in>
2022-01-22 12:47:59 +0530

    Day 1 project of 100 Days of Code

---
+++ Day1/Band_Name_Generator.py
@@ -0,0 +1,5 @@
+print("*************Band Name Generator***************")
+townName = input('Enter the Name of your Home town : ')
+petName = input('Enter the Name of your Pet : ')
+
+print('Your Band Name can be ' + townName , petName + '!!')
